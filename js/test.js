897b247895cc0ab1c5d1c24e5c41f8ec690be178
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2022-11-25 03:05:12 +0100

    first update slider section espaces

--- js/test.js
+++ js/test.js
@@ -12,44 +12,96 @@
 // });
 
 var lastScrollTop = 0;
-    navbar = document.getElementById("navbar");
-window.addEventListener("scroll", function (){
-    var scrollTop = window.pageYOffSet || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop){
-        navbar.style.top="-70px";
-    } else {
-        navbar.style.top ="0";
-    }
-    lastScrollTop = scrollTop;
+navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  var scrollTop = window.pageYOffSet || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-70px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
 })
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }))
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// ------------------------------------------- //
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+const carousel = document.querySelector(".carousel");
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".container_espaces_carrousel .w3-button")
+
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
+const showHideIcons = () => {
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+arrowIcons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14;
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60);
+  });
+})
+
+const autoSlide = () => {
+  if (carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return;
+
+  positionDiff = Math.abs(positionDiff);
+  let firstImgWidth = firstImg.clientWidth + 14;
+  let valDifference = firstImgWidth - positionDiff;
+
+  if (carousel.scrollLeft > prevScrollLeft) {
+    return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDIff;
   }
-  x[slideIndex-1].style.display = "block";
+  carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDIff;
 }
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  isDragging = false
+  carousel.classList.add("dragging");
+  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+}
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+
+  if (!isDragging) return;
+  isDragging = false;
+  autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging)
+carousel.addEventListener("touchmove", dragging)
+
+carousel.addEventListener("mouseup", dragStop)
+carousel.addEventListener("mouseleave", dragStop)
+carousel.addEventListener("touchend", dragStop)
